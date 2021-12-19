5a706972b3da7e945946bbaa9f43a97b2fc004d9
TanFlamer <zxtan01@outlook.com>
2021-12-19 22:28:52 +0800

    Removed HashMap and reworked unique code
    Removed HashMap and reworked unique code

--- FrequentFlyerProgram.java
+++ FrequentFlyerProgram.java
@@ -3,11 +3,15 @@ import java.util.*;
 public class FrequentFlyerProgram {
 
     private final TreeSet<Passenger> treeSet;
-    private final HashMap<String,int[]> confirmationList;
+    int referenceID;
 
     public FrequentFlyerProgram(){
         this.treeSet = new TreeSet<>(new Passenger());
-        this.confirmationList = new HashMap<>();
+        this.referenceID = 0;
+    }
+
+    public int getReferenceID(){
+        return referenceID++;
     }
 
     static class Passenger implements Comparator<Passenger> {
@@ -18,18 +22,20 @@ public class FrequentFlyerProgram {
         String confirmationCode;
         int flyerStatusCode;
         int waitingListTime;
+        int referenceID;
 
         String[] flyerStatusList = {"Silver","Gold","Platinum"};
 
         public Passenger(){}
 
-        public Passenger(String firstName,String lastName,String confirmationCode,int flyerStatusCode,int waitingListTime){
+        public Passenger(String firstName,String lastName,String confirmationCode,int flyerStatusCode,int waitingListTime,int referenceID){
             this.firstName = firstName;
             this.lastName = lastName;
             this.confirmationCode = confirmationCode;
             this.flyerStatusCode = flyerStatusCode;
             this.waitingListTime = waitingListTime;
             this.flyerStatus = flyerStatusList[flyerStatusCode];
+            this.referenceID = referenceID;
         }
 
         @Override
@@ -37,31 +43,38 @@ public class FrequentFlyerProgram {
             if(o1.flyerStatusCode != o2.flyerStatusCode){
                 return o2.flyerStatusCode - o1.flyerStatusCode;
             }
-            else {
+            else if (o1.waitingListTime != o2.waitingListTime){
                 return o2.waitingListTime - o1.waitingListTime;
+            }
+            else {
+                return o1.referenceID - o2.referenceID;
             }
         }
     }
 
-    public void addPassenger(String firstName,String lastName,String confirmationCode,int flyerStatusCode,int waitingListTime) {
-        String[] flyerStatusList = {"Silver","Gold","Platinum"};
-        if(!confirmationList.containsKey(confirmationCode)) {
-            if(treeSet.add(new Passenger(firstName,lastName,confirmationCode,flyerStatusCode,waitingListTime))) {
-                confirmationList.put(confirmationCode, new int[]{flyerStatusCode, waitingListTime});
-                System.out.format("Passenger %s %s with Confirmation Code %s has been added to the queue.\n", firstName, lastName, confirmationCode);
-            }
-            else
-                System.out.format("Passenger with %s rank and %d waiting time already exist in queue.\n",flyerStatusList[flyerStatusCode],waitingListTime);
+    public void addPassenger(String firstName,String lastName,int flyerStatusCode,int waitingListTime) {
+        int referenceID = getReferenceID();
+        char[] flyerStatusList = {'S','G','P'};
+        String flyerCode = String.format("%d",flyerStatusCode);
+        String reference = String.format("%02d",referenceID);
+        String waitingTime = String.format("%02d",waitingListTime);
+        String confirmationCode = flyerStatusList[flyerStatusCode] + flyerCode + reference + waitingTime;
+        boolean notExist = treeSet.add(new Passenger(firstName, lastName, confirmationCode, flyerStatusCode, waitingListTime, referenceID));
+
+        if(notExist) {
+            System.out.format("Passenger %s %s with Confirmation Code %s has been added to the queue.\n", firstName, lastName, confirmationCode);
         }
         else
             System.out.format("Passenger with Confirmation Code %s already exist in queue.\n",confirmationCode);
     }
 
     public void removePassenger(String confirmationCode){
-        int[] savedValues;
-        if(confirmationList.containsKey(confirmationCode)) {
-            savedValues = confirmationList.remove(confirmationCode);
-            treeSet.remove(new Passenger(null,null,null, savedValues[0], savedValues[1]));
+        int flyerCode = Integer.parseInt(confirmationCode.substring(1, 2));
+        int reference = Integer.parseInt(confirmationCode.substring(2, 4));
+        int waitingTime = Integer.parseInt(confirmationCode.substring(4, 6));
+        boolean exists = treeSet.remove(new Passenger(null, null, confirmationCode, flyerCode, waitingTime, reference));
+
+        if(exists) {
             System.out.format("Passenger with Confirmation Code %s has been removed from the queue.\n",confirmationCode);
         }
         else
@@ -70,9 +83,9 @@ public class FrequentFlyerProgram {
 
     public void printList(){
         if(!treeSet.isEmpty()) {
-            System.out.format("%-12s%-12s%-14s%-14s%-12s\n", "First Name", "Last Name", "Booking Code", "Flyer Status", "Waiting Time");
+            System.out.format("%-2s  %-12s%-12s%-14s%-14s%-12s\n","ID", "First Name", "Last Name", "Booking Code", "Flyer Status", "Waiting Time");
             for (Passenger current : treeSet) {
-                System.out.format("%-12s%-12s%-14s%-14s%-12d\n", current.firstName, current.lastName, current.confirmationCode, current.flyerStatus, current.waitingListTime);
+                System.out.format("%02d  %-12s%-12s%-14s%-14s%-12d\n",current.referenceID, current.firstName, current.lastName, current.confirmationCode, current.flyerStatus, current.waitingListTime);
             }
         }
         else{
@@ -85,7 +98,6 @@ public class FrequentFlyerProgram {
             if(!treeSet.isEmpty()) {
                 Passenger passenger = treeSet.pollFirst();
                 assert passenger != null;
-                confirmationList.remove(passenger.confirmationCode);
                 System.out.format("Passenger %s %s with %s flyer status has selected from the queue.\n", passenger.firstName, passenger.lastName, passenger.flyerStatus);
             }
             else {
@@ -96,16 +108,17 @@ public class FrequentFlyerProgram {
     }
 
     public void initialisePassengerList(FrequentFlyerProgram frequentFlyerProgram){
-        frequentFlyerProgram.addPassenger("Charles","Leong","KA200",1,10);
-        frequentFlyerProgram.addPassenger("Tan","Xian","KA201",2,20);
-        frequentFlyerProgram.addPassenger("John","Cena","KA203",0,20);
-        frequentFlyerProgram.addPassenger("Alton","John","KA204",2,10);
-        frequentFlyerProgram.addPassenger("John","John","KA205",1,20);
-        frequentFlyerProgram.addPassenger("Smith","John","KA206",0,15);
-        frequentFlyerProgram.addPassenger("Harry","Potter","KA207",0,25);
-        frequentFlyerProgram.addPassenger("Albus","Snape","KA208",2,12);
-        frequentFlyerProgram.addPassenger("Merry","Santa","KA209",1,27);
-        frequentFlyerProgram.addPassenger("John","Smith","KA202",0,10);
+        frequentFlyerProgram.addPassenger("Charles","Leong",1,10);
+        frequentFlyerProgram.addPassenger("Tan","Xian",2,20);
+        frequentFlyerProgram.addPassenger("John","Cena",0,20);
+        frequentFlyerProgram.addPassenger("Alton","John",2,10);
+        frequentFlyerProgram.addPassenger("John","John",1,20);
+        frequentFlyerProgram.addPassenger("Smith","John",0,15);
+        frequentFlyerProgram.addPassenger("Harry","Potter",0,25);
+        frequentFlyerProgram.addPassenger("Albus","Snape",2,12);
+        frequentFlyerProgram.addPassenger("Merry","Santa",1,27);
+        frequentFlyerProgram.addPassenger("John","Smith",0,10);
+        frequentFlyerProgram.addPassenger("John","Smith",0,10);
     }
 
     public static void main(String[] args) {
@@ -136,10 +149,6 @@ public class FrequentFlyerProgram {
                     System.out.println("Enter Passenger Last Name:");
                     lastName = input.next();
                     do {
-                        System.out.println("Enter Passenger Confirmation Code:");
-                        confirmationCode = input.next();
-                    } while (confirmationCode.length() != 5);
-                    do {
                         System.out.println("Enter Passenger Flyer Status (0-Silver|1-Gold|2-Platinum):");
                         flyerStatusCode = input.nextInt();
                     } while (flyerStatusCode < 0 || flyerStatusCode > 2);
@@ -147,13 +156,13 @@ public class FrequentFlyerProgram {
                         System.out.println("Enter Passenger Waiting Time:");
                         waitingListTime = input.nextInt();
                     } while (waitingListTime < 0);
-                        frequentFlyerProgram.addPassenger(firstName, lastName, confirmationCode, flyerStatusCode, waitingListTime);
+                        frequentFlyerProgram.addPassenger(firstName, lastName, flyerStatusCode, waitingListTime);
                 }
                 case 2 -> {
                     do {
                         System.out.println("Enter Passenger Confirmation Code:");
                         confirmationCode = input.next();
-                    } while (confirmationCode.length() != 5);
+                    } while (confirmationCode.length() != 6);
                     frequentFlyerProgram.removePassenger(confirmationCode);
                 }
                 case 3 -> frequentFlyerProgram.printList();
